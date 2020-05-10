b824fa1d86cfc55f7e2d77c9299ea82c21ccba63
Keeley Hoek <keeley@hoek.io>
2020-05-10 23:12:41 +1000

    Give criterion a go

---
+++ benches/suites.rs
@@ -0,0 +1,33 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use kcpu::frontend::{
+    assemble,
+    run::execute::{self, AbortAction, Config, Verbosity},
+};
+use std::path::PathBuf;
+
+fn suite_test_primes(c: &mut Criterion) {
+    // RUSTFIX proper error handling, instead of just calling `unwrap()`.
+    let prog_bin = assemble::assemble_path(&PathBuf::from("asm/test/primes.ks")).unwrap();
+
+    c.bench_function("sample", |b| {
+        b.iter(|| {
+            execute::execute(
+                Config {
+                    headless: true,
+                    max_clocks: None,
+                    abort_action: AbortAction::Stop,
+
+                    verbosity: Verbosity::Silent,
+                    print_marginals: false,
+                },
+                None,
+                Some(&prog_bin),
+            )
+        })
+    });
+}
+
+// RUSTFIX pre-compile and the run all of the units in the `test` suite (`bench` will take too long I think)
+
+criterion_group!(benches, suite_test_primes);
+criterion_main!(benches);
